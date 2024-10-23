6e405dd71b1f7d8df59a5d80007cdfb891732ca2
b_g <b_g@dsm.hs.kr>
2024-10-23 13:24:29 +0900

    design - input

--- lib/design/input.dart
+++ lib/design/input.dart
@@ -8,7 +8,23 @@ class InputText extends StatefulWidget {
   final int? maxLength;
   final String placeholder;
   final int? defaultLine;
-  const InputText({super.key, this.onChanged, this.onSubmitted, this.controller, this.maxLength, this.placeholder = "", this.defaultLine});
+  final BorderRadius radius;
+  final EdgeInsets padding;
+  final double? lineHeight;
+  final Widget? prefixIcon;
+  const InputText({
+    super.key,
+    this.onChanged,
+    this.onSubmitted,
+    this.controller,
+    this.maxLength,
+    this.placeholder = "",
+    this.defaultLine,
+    this.radius = const BorderRadius.all(Radius.circular(5)),
+    this.padding = const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+    this.lineHeight,
+    this.prefixIcon
+  });
 
   @override
   State<InputText> createState() => _InputTextState();
@@ -17,26 +33,36 @@ class InputText extends StatefulWidget {
 class _InputTextState extends State<InputText> {
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: widget.controller,
-      onChanged: widget.onChanged??(_){},
-      onSubmitted: widget.onSubmitted??(_){},
-      maxLength: widget.maxLength,
-      cursorColor: Colors.black,
-      cursorWidth: 1,
-      cursorHeight: 16,
-      maxLines: (widget.defaultLine != null) ? null : 1,
-      minLines: widget.defaultLine,
-      decoration: InputDecoration(
-        filled: true,
-        fillColor: gray1,
-        hoverColor: gray1,
-        focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: gray2, width: 2)),
-        border: const OutlineInputBorder(borderSide: BorderSide(color: gray2, width: 2)),
-        enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: gray2, width: 2)),
-        hintText: widget.placeholder,
-        hintStyle: const TextStyle(color: gray5),
-        counterText: ''
+    return TextSelectionTheme(
+      data: TextSelectionThemeData(
+        selectionColor: main3.withOpacity(0.5),
+        selectionHandleColor: main2
+      ),
+      child: TextField(
+        controller: widget.controller,
+        onChanged: widget.onChanged??(_){},
+        onSubmitted: widget.onSubmitted??(_){},
+        maxLength: widget.maxLength,
+        cursorColor: Colors.black,
+        cursorWidth: 1,
+        cursorHeight: 16,
+        maxLines: (widget.defaultLine != null) ? null : 1,
+        minLines: widget.defaultLine,
+        style: TextStyle(height: widget.lineHeight),
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: gray1,
+          hoverColor: gray1,
+          isDense: true,
+          prefixIcon: widget.prefixIcon,
+          contentPadding: widget.padding,
+          focusedBorder: OutlineInputBorder(borderSide: const BorderSide(color: gray2, width: 2), borderRadius: widget.radius),
+          border: OutlineInputBorder(borderSide: const BorderSide(color: gray2, width: 2), borderRadius: widget.radius),
+          enabledBorder: OutlineInputBorder(borderSide: const BorderSide(color: gray2, width: 2), borderRadius: widget.radius),
+          hintText: widget.placeholder,
+          hintStyle: const TextStyle(color: gray3),
+          counterText: ''
+        )
       )
     );
   }
